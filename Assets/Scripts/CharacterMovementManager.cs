69e44f6b4cb4dcbf4e1fa5de23a258c2ff5e361e
Yusuf Gözüm <139639043+gozumyusuff@users.noreply.github.com>
2024-08-04 21:40:11 +0300

    Initial commit

---
+++ Assets/Scripts/CharacterMovementManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovementManager : MonoBehaviour
+{
+   
+    public VariableJoystick joystick;
+    //public CharacterController controller;
+    public Rigidbody2D rb;
+    public float movementSpeed;
+
+    public Canvas inputCanvas;
+    public bool isJoystick;
+
+    private void Start()
+    {
+        EnableJoystickInput();
+    }
+
+    public void EnableJoystickInput()
+    {
+        isJoystick = true;
+        inputCanvas.gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (isJoystick)
+        {
+            var movementDirection = new Vector2(joystick.Direction.x, joystick.Direction.y);
+            rb.velocity = movementDirection * movementSpeed;
+        }
+    }
+
+
+
+}
